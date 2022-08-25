d787dc3b3da1dd17e150cb3a530ed38e113461be
vfdesouza <vfdesouza@outlook.com>
2022-08-25 07:09:40 -0300

    inicialização do projeto

---
+++ mudi/src/main/java/br/com/mudi/MudiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiApplication.class, args);
+	}
+
+}
