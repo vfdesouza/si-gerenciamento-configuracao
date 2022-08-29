8747218ed08a1619c513a7f90bf06147da2d6310
Renan Da Cruz <waynerenan@hotmail.com>
2022-08-29 16:33:55 -0300

    Mensagem descritiva do commit

---
+++ mudi/src/main/java/br/com/mudi/RequestController.java/RequestController.java
@@ -0,0 +1,24 @@
+package br.com.mudi.controllers;
+
+import br.com.mudi.models.Request;
+import br.com.mudi.models.StatusRequest;
+import br.com.mudi.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("formCad")
+public class RequestController {
+    @Autowired
+    private RequestRepository rr;
+
+    @GetMapping
+    public String form(Request request) {
+        return "formCad";
+    }
+
+}
